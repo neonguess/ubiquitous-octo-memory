b777931737b9f34e2c0997bd34ee2bf5a7fd9c27
SavoyeAdrien <85487843+SavoyeAdrien@users.noreply.github.com>
2021-06-22 15:44:48 +0200

    Update Page de jeu adrien.php
    voila on a les X et les Y des points mais aussi le R2 qui est calculé. si vous voulez vérifier chargez la page et il sera affiché

--- Page de jeu adrien.php
+++ Page de jeu adrien.php
@@ -45,8 +45,8 @@ diffcor == Corrélation-R²
 
 <?php
 
-  $RandMax = 50;
-  $RandMin = -50;
+  $RandMax = random_int(1,100);
+  $RandMin = random_int(-100,-1);
 
   $P1X= 0.05;
   $P1Y= $P1X+ (random_int($RandMin,$RandMax)/100);
@@ -108,12 +108,97 @@ diffcor == Corrélation-R²
   $P20X= 1;
   $P20Y=  $P20X+ (random_int($RandMin,$RandMax)/100);
 
+  $V1 = $P1Y-$P1X;
+  $V2 = $P2Y-$P2X;
+  $V3 = $P3Y-$P3X;
+  $V4 = $P4Y-$P4X;
+  $V5 = $P5Y-$P5X;
+  $V6 = $P6Y-$P6X;
+  $V7 = $P7Y-$P7X;
+  $V8 = $P8Y-$P8X;
+  $V9 = $P9Y-$P9X;
+  $V10 = $P10Y-$P10X;
+  $V11 = $P11Y-$P11X;
+  $V12 = $P12Y-$P12X;
+  $V13 = $P13Y-$P13X;
+  $V14 = $P14Y-$P14X;
+  $V15 = $P15Y-$P15X;
+  $V16 = $P16Y-$P16X;
+  $V17 = $P17Y-$P17X;
+  $V18 = $P18Y-$P18X;
+  $V19 = $P19Y-$P19X;
+  $V20 =  $P20Y-$P20X;
+
+  $V1 =  $V1 * $V1;
+  $V2 =  $V2 * $V2;
+  $V3 =  $V3 * $V3;
+  $V4 =  $V4 * $V4;
+  $V5 =  $V5 * $V5;
+  $V6 =  $V6 * $V6;
+  $V7 =  $V7 * $V7;
+  $V8 =  $V8 * $V8;
+  $V9 =  $V9 * $V9;
+  $V10 = $V10 * $V10;
+  $V11 = $V11 * $V11;
+  $V12 = $V12 * $V12;
+  $V13 = $V13 * $V13;
+  $V14 = $V14 * $V14;
+  $V15 = $V15 * $V15;
+  $V16 = $V16 * $V16;
+  $V17 = $V17 * $V17;
+  $V18 = $V18 * $V18;
+  $V19 = $V19 * $V19;
+  $V20 = $V20 * $V20;
+
+  $MEANY = ($P1Y+$P2Y+$P3Y+$P4Y+$P5Y+$P6Y+$P7Y+$P8Y+$P9Y+$P10Y+$P11Y+$P12Y+$P13Y+$P14Y+$P15Y+$P16Y+$P17Y+$P18Y+$P19Y+$P20Y)/20;
+
+  $W1 = $P1Y-$MEANY;
+  $W2 = $P2Y-$MEANY;
+  $W3 = $P3Y-$MEANY;
+  $W4 = $P4Y-$MEANY;
+  $W5 = $P5Y-$MEANY;
+  $W6 = $P6Y-$MEANY;
+  $W7 = $P7Y-$MEANY;
+  $W8 = $P8Y-$MEANY;
+  $W9 = $P9Y-$MEANY;
+  $W10 = $P10Y-$MEANY;
+  $W11 = $P11Y-$MEANY;
+  $W12 = $P12Y-$MEANY;
+  $W13 = $P13Y-$MEANY;
+  $W14 = $P14Y-$MEANY;
+  $W15 = $P15Y-$MEANY;
+  $W16 = $P16Y-$MEANY;
+  $W17 = $P17Y-$MEANY;
+  $W18 = $P18Y-$MEANY;
+  $W19 = $P19Y-$MEANY;
+  $W20 = $P20Y-$MEANY;
+
+  $W1 =  $W1 * $W1;
+  $W2 =  $W2 * $W2;
+  $W3 =  $W3 * $W3;
+  $W4 =  $W4 * $W4;
+  $W5 =  $W5 * $W5;
+  $W6 =  $W6 * $W6;
+  $W7 =  $W7 * $W7;
+  $W8 =  $W8 * $W8;
+  $W9 =  $W9 * $W9;
+  $W10 = $W10 * $W10;
+  $W11 = $W11 * $W11;
+  $W12 = $W12 * $W12;
+  $W13 = $W13 * $W13;
+  $W14 = $W14 * $W14;
+  $W15 = $W15 * $W15;
+  $W16 = $W16 * $W16;
+  $W17 = $W17 * $W17;
+  $W18 = $W18 * $W18;
+  $W19 = $W19 * $W19;
+  $W20 = $W20 * $W20;
 
 
-  echo  " <p>X1 = $P1X </p>";
-  echo " <p>Y1 = $P1Y </p>";
-  echo  " <p>X15 = $P15X </p>";
-  echo " <p>Y15 = $P15Y </p>";
+  $C= $W1+$W2+$W3+$W4+$W5+$W6+$W7+$W8+$W9+$W10+$W11+$W12+$W13+$W14+$W15+$W16+$W17+$W18+$W19+$W20;
+  $B= $V1+$V2+$V3+$V4+$V5+$V6+$V7+$V8+$V9+$V10+$V11+$V12+$V13+$V14+$V15+$V16+$V17+$V18+$V19+$V20;
+  $R2= ($C-$B)/$C;
+  echo "  <h1> $R2 </h1>";
  ?>
 
 <!-- NOTE POUR ADRIEN : Utiliser l'URL :"http://localhost/projet%20S2/Page%20de%20jeu%20(1).php" pour que ca marche-->
